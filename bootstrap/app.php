d0a71a4b09a5abe282c7abc04270c85e10d6db04
trivaldo0858 <trivaldo432@gmail.com>
2026-04-19 09:52:03 +0700

    ubah dikit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,10 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        // Daftarkan alias middleware di sini
-        $middleware->alias([
-            'role' => \App\Http\Middleware\RoleMiddleware::class,
+    ->withMiddleware(function (Middleware $middleware) {
+    $middleware->alias([
+        'role' => \App\Http\Middleware\RoleMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
